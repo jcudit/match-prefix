1982edcbb7abd711b1bed3c6496609e8802698c6
Jeremy Udit <jcudit@github.com>
2020-02-25 20:49:40 -0500

    Adds comment body output

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,7 @@ echo $(comment_body) | egrep "^${INPUT_PREFIX} " || exit 1
 
 echo "Setting Successful Outputs On Match ..."
 echo ::set-output name=result::true
+echo ::set-output name=comment_body::$(comment_body)
 echo ::set-output name=actions_url::$(actions_url)
 echo ::set-output name=issue_number::$(issue_number)
 echo ::set-output name=issue_actor::${GITHUB_ACTOR}
